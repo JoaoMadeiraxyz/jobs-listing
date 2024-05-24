a2fde2b807f8ccfd7b264e90682ef003908ffa44
JoaoMadeiraxyz <jaodisc@gmail.com>
2024-05-23 23:32:58 -0300

    feat: add list of jobs

--- jobs-list/src/app/layout.jsx
+++ jobs-list/src/app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-gray-950 text-white">{children}</body>
     </html>
   );
 }
--- jobs-list/src/app/page.jsx
+++ jobs-list/src/app/page.jsx
@@ -1,6 +1,16 @@
+import { JobsList } from "@/components/JobsList";
+
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center justify-center">
+      <header className="flex flex-col items-center justify-center pt-24 pb-14 text-center gap-5">
+        <h1 className="font-bold text-5xl">My Jobs List</h1>
+        <p className="font-normal max-w-[500px]">
+          Welcome to my jobs list. This project was made with the intention of
+          listing jobs all around the world.
+        </p>
+      </header>
+      <JobsList />
     </main>
   );
 }
